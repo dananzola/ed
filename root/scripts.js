531423924dc862cc73862fc2be10f36438d69dac
dananzola <dananzola@gmail.com>
2016-09-28 20:15:11 -0400

    Add files via upload

---
+++ root/scripts.js
@@ -0,0 +1,6 @@
+var userFirstName = prompt("What is your first name?");
+var userLastName = prompt("What is your last name?");
+var userAge = prompt("May i ask how old you are?");
+alert("Nice to meet you, " + userFirstName + " " + userLastName + ". " + userAge + " was my favorite! ");
+console.log("Also nice to meet you, " + userFirstName);
+alert("you are " + userAge*365 + " days old!" )
